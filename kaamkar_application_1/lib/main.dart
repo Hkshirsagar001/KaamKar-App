cf1a008095b8f23463e25bb189111763d831d307
Harshkshirsagar0 <harshkshirsagar2003@gmail.com>
2024-10-28 17:04:26 +0530

    harsh

--- kaamkar_application_1/lib/main.dart
+++ kaamkar_application_1/lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:kaamkar_application_1/screen1.dart';
-import "screen2.dart";
-import 'screen3.dart';
-import 'screen4.dart';
-import 'screen5.dart';
-import 'screen6.dart';
-import 'screen7.dart';
 
 void main() {
   runApp(const MainApp());
